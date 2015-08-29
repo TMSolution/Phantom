230085345dde29f6431aa5789318c5861b78f9f5
Kamil <serwis@tmsolution.pl>
2015-08-29 21:30:09 +0200

    Changed names - 'object' to 'gamer'
    Conflicts:
    	Entity/Objecttrophy.php

--- Entity/Product.php
+++ Entity/Product.php
@@ -1,16 +1,64 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace PhantomBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * Description of Product
- *
- * @author Mariusz Piela <mariusz.piela@tmsolution.pl>
+ * @ORM\Entity
+ * @ORM\Table(name="phantom_product")
  */
 class Product {
-    //put your code here
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $price;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="ProductCategory", inversedBy="product")
+     * @ORM\JoinTable(name="phantom_pruduct_has_category")
+     */
+    protected $productCategories;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ProductCategory")
+     * @ORM\JoinColumn(name="product_category_id", referencedColumnName="id")
+     */
+    protected $mainProductCategory;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="ProductProperty", mappedBy="product")
+     */
+    protected $productProperties;
+    
+    
+    public function __construct() {
+        
+        $this->productCategories= new ArrayCollection;
+        $this->productProperties= new ArrayCollection;
+        
+    }
+    
+    public function __toString() {
+        return $this->name;
+    }
+    
+    
+    
+    
+
 }
