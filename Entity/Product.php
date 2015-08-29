55e346f5f988f5bb33b3a6b10c39fb1bd8657acb
Kamil <serwis@tmsolution.pl>
2015-08-29 20:28:04 +0200

    Changed names - 'object' to 'gamer'
    Conflicts:
    	Entity/Objecttrophy.php

---
+++ Entity/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Product
+ *
+ * @author Mariusz Piela <mariusz.piela@tmsolution.pl>
+ */
+class Product {
+    //put your code here
+}
